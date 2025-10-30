88b772195733f54012aab6e006dbcd3eb81e4c7f
Jared Rueter <jrueter15@gmail.com>
2025-10-30 12:55:29 -0500

    Add enabled column to users table and unique index to authorities

--- backend/src/main/resources/db/migration/V1__create_user_table.sql
+++ backend/src/main/resources/db/migration/V1__create_user_table.sql
@@ -1,13 +1,15 @@
 CREATE TABLE IF NOT EXISTS users (
   id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(50) NOT NULL UNIQUE,
-  password VARCHAR(500) NOT NULL
+  password VARCHAR(500) NOT NULL,
+  enabled BOOLEAN NOT NULL DEFAULT TRUE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS authorities (
-  user_id BIGINT NOT NULL,
+  username VARCHAR(50) NOT NULL,
   authority VARCHAR(50) NOT NULL,
-  PRIMARY KEY (user_id, authority),
-  CONSTRAINT fk_auth_user FOREIGN KEY (user_id) REFERENCES users(id)
+  CONSTRAINT fk_authorities_users FOREIGN KEY(username) REFERENCES users(username)
     ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE UNIQUE INDEX ix_auth_username ON authorities (username, authority);
