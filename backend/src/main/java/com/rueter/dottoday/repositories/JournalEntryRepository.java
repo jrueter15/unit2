2d670e5ca295f8966f7ac7c32b05c4101b4594c6
Jared Rueter <jrueter15@gmail.com>
2025-11-06 18:29:50 -0600

    create database query

--- backend/src/main/java/com/rueter/dottoday/repositories/JournalEntryRepository.java
+++ backend/src/main/java/com/rueter/dottoday/repositories/JournalEntryRepository.java
@@ -3,8 +3,10 @@ package com.rueter.dottoday.repositories;
 import com.rueter.dottoday.models.JournalEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface JournalEntryRepository extends JpaRepository<JournalEntry, Long> {
     List<JournalEntry> findByUserId(Long userId);
+    List<JournalEntry> findByUserIdAndDateCreatedAfter(Long userId, LocalDateTime dateCreated);
 }
