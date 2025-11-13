423b287becbde66c61650d132e10aa6ac20b2b50
Jared Rueter <jrueter15@gmail.com>
2025-11-13 10:12:03 -0600

    App working again - controller mapping issue

--- backend/src/main/java/com/rueter/dottoday/controllers/JournalEntryController.java
+++ backend/src/main/java/com/rueter/dottoday/controllers/JournalEntryController.java
@@ -13,7 +13,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/journal-entries")
+@RequestMapping("api/journal-entries")
 public class JournalEntryController {
 
     @Autowired
